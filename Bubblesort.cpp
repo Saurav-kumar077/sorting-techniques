fc9d086c881fdeb2519c5378856ac5aa6ce69664
Saurav-kumar077 <126255878+Saurav-kumar077@users.noreply.github.com>
2025-02-15 15:10:10 +0530

    bubblesort

---
+++ Bubblesort.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void Bubblesort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j+1],arr[j]);
+            }
+        }
+    }
+}
+void printarray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+}
+int main(){
+    int arr [5]={64,25,12,22,11};
+    cout<<"The original array is "<<endl;
+    printarray(arr,5);
+    cout<<"The sorted array is "<<endl;
+    Bubblesort(arr,5);
+    printarray(arr,5);
+
+    
+}
