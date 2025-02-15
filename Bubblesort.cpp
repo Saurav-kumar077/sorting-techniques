eff5b909dce540d0e2b6243c46e8029a1391da5f
Saurav-kumar077 <126255878+Saurav-kumar077@users.noreply.github.com>
2025-02-15 15:20:29 +0530

    moreoptimizes

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -2,11 +2,17 @@
 using namespace std;
 void Bubblesort(int arr[],int n){
     for(int i=0;i<n-1;i++){
+        bool swapped=false;
         for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j+1],arr[j]);
+                swapped=true;
             }
         }
+        if(swapped==false){
+            break;
+        }
+
     }
 }
 void printarray(int arr[],int n){
